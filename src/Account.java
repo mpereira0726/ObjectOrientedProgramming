47e6f2fbbc4addfbb6bb61f990a18b101a505838
mpereira0726 <106694216+mpereira0726@users.noreply.github.com>
2022-06-30 14:50:00 -0400

    Accountt

---
+++ src/Account.java
@@ -0,0 +1,25 @@
+public class Account {
+    private String id;
+    private String name;
+    private double balance;
+
+
+    public void Account()
+    {
+        this.id = id;
+        this.name = name;
+    }
+    public void Account()
+    {
+
+    }
+    public void credit()
+    {
+
+    }
+
+    public void debit()
+    {
+
+    }
+}
